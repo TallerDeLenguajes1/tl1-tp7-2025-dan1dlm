ba990b8e7d7e68284e7b8492e4a67c51d20cf7e6
“dan1dlm” <daniel01mamani01gmail.com>
2025-06-01 23:24:58 -0300

    ejercicio 2 realizado

---
+++ SisPersonal/SisPersonal.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using system;
+
+public enum Cargos
+{
+    Auxiliar,
+    Administrativo,
+    Ingeniero,
+    Especialista,
+    Investigador
+}
+public class Empleado
+{
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public char EstadoCivil { get; set; }
+    public DateTime FechaIngreso { get; set; }
+    public double SueldoBasico { get; set; }
+    public Cargos Cargo { get; set; }
+}
