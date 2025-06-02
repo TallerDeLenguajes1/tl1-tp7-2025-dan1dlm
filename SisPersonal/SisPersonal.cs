62fb235f1a2a659ce3abb4eae726526ab6fb3458
“dan1dlm” <daniel01mamani01gmail.com>
2025-06-02 00:03:38 -0300

    carga de usuarios realizada

--- SisPersonal/SisPersonal.cs
+++ SisPersonal/SisPersonal.cs
@@ -58,3 +58,50 @@ public class Empleado
         return SueldoBasico + adicional;
     }
 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Empleado[] empleados = new Empleado[3];
+
+        empleados[0] = new Empleado
+        {
+            Nombre = "Juan",
+            Apellido = "Pérez",
+            FechaNacimiento = new DateTime(1980, 5, 10),
+            EstadoCivil = 'C',
+            FechaIngreso = new DateTime(2010, 3, 15),
+            SueldoBasico = 650000,
+            Cargo = Cargos.Ingeniero
+        };
+
+        empleados[1] = new Empleado
+        {
+            Nombre = "Ana",
+            Apellido = "García",
+            FechaNacimiento = new DateTime(1990, 8, 22),
+            EstadoCivil = 'S',
+            FechaIngreso = new DateTime(2015, 7, 1),
+            SueldoBasico = 500000,
+            Cargo = Cargos.Administrativo
+        };
+
+        empleados[2] = new Empleado
+        {
+            Nombre = "Luis",
+            Apellido = "Martínez",
+            FechaNacimiento = new DateTime(1975, 12, 3),
+            EstadoCivil = 'C',
+            FechaIngreso = new DateTime(2000, 1, 20),
+            SueldoBasico = 800000,
+            Cargo = Cargos.Investigador
+        };
+
+        
+        foreach (var emp in empleados)
+        {
+            Console.WriteLine($"{emp.Nombre} {emp.Apellido} - Cargo: {emp.Cargo}");
+        }
+    }
+}
